e276aa1b3ab4ee6ad2695e2215e087174147711c
Roland Tepp <luolong@gmail.com>
2016-01-15 14:10:35 +0200

    First failing test

---
+++ src/test/java/info/tepp/parser/earley/GrammarTest.java
@@ -0,0 +1,14 @@
+package info.tepp.parser.earley;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class GrammarTest {
+
+    @Test
+    public void test() throws Exception {
+        fail("Not implemented!");
+    }
+
+}
